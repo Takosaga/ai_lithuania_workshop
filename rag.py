6874990a9ab40495289e37f8dd2f902768e42162
Gonzalo Gamez <takosaga@gmail.com>
2025-02-22 12:37:05 +0200

    feat: rag stuff, need to work on

--- rag.py
+++ rag.py
@@ -1,5 +1,39 @@
 from praisonaiagents import Agent
 
-agent = Agent(instructions="You are helpful Assisant", llm="qwen2.5:7b")
+config = {
+    "vector_store": {
+        "provider": "chroma",
+        "config": {
+            "collection_name": "praison",
+            "path": ".praison"
+        }
+    },
+    "llm": {
+        "provider": "ollama",
+        "config": {
+            "model": "qwen2.5:7b",
+            "temperature": 0,
+            "max_tokens": 8000,
+            "ollama_base_url": "http://localhost:11434",
+        },
+    },
+    "embedder": {
+        "provider": "ollama",
+        "config": {
+            "model": "nomic-embed-text:latest",
+            "ollama_base_url": "http://localhost:11434",
+            "embedding_dims": 1536
+        },
+    },
+}
 
-agent.start("Why sky is Blue?")
+agent = Agent(
+    name="Knowledge Agent",
+    instructions="You answer questions based on the provided knowledge.",
+    knowledge=["file.txt"], # Indexing
+    knowledge_config=config,
+    user_id="user1",
+    llm="qwen2.5:7b"
+)
+
+agent.start("What is in the file.txt?") # Retrieval
