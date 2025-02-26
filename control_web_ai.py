c70ca1a1580a5b2450fe9215a4c3c59349bc3525
Gonzalo Gamez <takosaga@gmail.com>
2025-02-26 20:39:37 +0200

    feat: update ai web crawl

--- control_web_ai.py
+++ control_web_ai.py
@@ -1,6 +1,13 @@
 import os
 os.environ["ANONYMIZED_TELEMETRY"] = "false"
 
+from dotenv import load_dotenv
+# loading variables from .env file
+load_dotenv() # pass a path if it's not a .env in the current working directory 
+ 
+# accessing and printing value
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 import asyncio
 
 from browser_use import Agent
